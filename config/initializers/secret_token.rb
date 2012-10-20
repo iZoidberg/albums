84555a5da4eca8317a99ab2a8ec924660b896b7f
Pawel Cofta <pawel.cofta@cocktailscout.de>
2012-10-20 16:53:41 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albums::Application.config.secret_token = 'c4a9df6942fac8123d476114a0005f857277e53512a685c373bec29697d5ab00c7612d294891c7530b519ad613faae82efdc3a98dec8aba76dbd60441ef2a712'
